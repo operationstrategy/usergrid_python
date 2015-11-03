c5cffcc6354fa1616059a2b9660352bbc65682fa
Chris Smith <chris.smith@iconinteract.com>
2015-11-03 10:12:01 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='UserGrid',
+    version='0.1',
+    description='UserGrid 1.x Client',
+    author='Christopher Smith',
+    author_email='chris.s@bigmirrorlabs.com',
+    py_modules=['usergrid']
+)
