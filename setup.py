9f94c79c5034511f4ab862e52f57ddd40c056e39
Ubuntu <ubuntu@ip-10-145-18-33.ec2.internal>
2015-11-03 19:09:39 +0000

    use setuptools, not distutils

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='UserGrid',
     version='0.1',
     description='UserGrid 1.x Client',
     author='Christopher Smith',
     author_email='chris.s@bigmirrorlabs.com',
-    py_modules=['usergrid/usergrid']
+    packages=['usergrid'],
+    include_package_data = True
 )
