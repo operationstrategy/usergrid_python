d826472e9bee5c483bba375262abe187cc140664
Chuck "MANCHUCK" Reeves <chuck@manchuck.com>
2017-11-06 14:36:50 -0500

    Fixing dependencies and adding usergrid to __init__

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 setup(
     name='UserGrid',
-    version='0.1.4',
+    version=__version__,
     description='UserGrid 1.x Client',
     author='Christopher Smith',
     author_email='chris.s@bigmirrorlabs.com',
--- usergrid/__init__.py
+++ usergrid/__init__.py
@@ -1,2 +1,6 @@
+from .usergrid import UserGrid
+
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__all__ = ['UserGrid']
