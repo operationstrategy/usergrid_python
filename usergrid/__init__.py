72f785e13d1be9b1e5b8ea40c8b799f49f2a34d1
Chuck "MANCHUCK" Reeves <chuck@manchuck.com>
2017-12-01 13:08:44 -0500

    Update Password Method
    Added UserGrid.update_password method
    Updated __init__ allowing exporting all classes
    Version Bump

--- usergrid/__init__.py
+++ usergrid/__init__.py
@@ -1,5 +1,11 @@
 from .usergrid import *
+from .mock_usergrid import MockUserGrid
 
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
+__all__ = [
+    UserGridException,
+    UserGrid,
+    MockUserGrid
+]
